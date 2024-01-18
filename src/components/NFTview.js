68613f4f844aae211c12fe8a700094486f9b84b7
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-01-18 19:10:17 +0100

    Update

--- src/components/NFTview.js
+++ src/components/NFTview.js
@@ -2,13 +2,12 @@ import React from 'react';
 import styles from "./NFTview.module.css"
 import Image from "next/image";
 import NFT from "../../public/preview.gif";
-const NfTview = ({ isOpen, onClose, children }) => {
+const NfTview = ({ isOpen, onClose}) => {
     if (!isOpen) return null;
 
     return (
         <div className={styles.modalBackdrop} onClick={onClose}>
             <div className={styles.modalContent} onClick={e => e.stopPropagation()}>
-                {children}
                 <Image src={NFT} alt="NFT Fullscreen" layout="fill" objectFit="contain" loading="eager"/>
 
             </div>
